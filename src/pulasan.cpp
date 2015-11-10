9e55c03bab4149fe8406be8d39cffa854911286e
Legonigel <legonigel@gmail.com>
2015-11-09 23:25:17 -0500

    Code cleanup and better command line things

--- src/pulasan.cpp
+++ src/pulasan.cpp
@@ -2,6 +2,7 @@
 #include <modbus.h>
 #include <stdio.h>
 #include <iostream>
+#include <sys/ioctl.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <exception>
@@ -21,14 +22,17 @@ public:
   int S;
 };
 
+/* used for ctrl-c interupt */
 void sig_to_exception(int s)
 {
   throw InterruptException(s);
 }
 
 int main(){
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
     modbus_t *mb;
-    uint16_t tab_reg[32];
 
     mb = modbus_new_tcp(IP_ADDR, 502);
     if (mb == NULL) {
@@ -41,19 +45,19 @@ int main(){
         return -1;
     }
 
-    struct sigaction sigIntHandler;
-    sigIntHandler.sa_handler = sig_to_exception;
-    sigemptyset(&sigIntHandler.sa_mask);
-    sigIntHandler.sa_flags = 0;
-    sigaction(SIGINT, &sigIntHandler, NULL);
+    setup_interupt();
 
     uint16_t input = 0;
     uint8_t outputs[16] = {0};
     bool write_bool = true;
     int  write_num  = 0;
     int  count      = 0;
-
-    std::cout << "Project Pulasan" << std::endl << "Press CTRL-C to exit" << std::endl << std::endl << std::endl << std::endl;
+    for(int i = 0; i <w.ws_col; i++)
+        std::cout << "=";
+    std::cout << std::endl << "Project Pulasan" << std::endl << "Press CTRL-C to exit" << std::endl;
+    for(int i = 0; i <w.ws_col; i++)
+        std::cout << "=";
+    std::cout<< std::endl << std::endl << std::endl << std::endl;
 
     try{
         while (true) {
@@ -79,8 +83,13 @@ int main(){
             // use \e[A to move up a line
             std::cout << "\r\e[A\e[A\e[A"
                       << "         0123456789012345" << std::endl;
-            std::cout << "inputs:  " << std::bitset<16>(input) << std::endl;
-            std::cout << "outputs: ";
+            std::cout << "inputs:  ";
+            std::bitset<16> inputs(input);
+            for(int i = 0; i < 16; i++){
+                std::cout << inputs[i];
+            }
+            std::cout << std::endl
+                      << "outputs: ";
             for(int i = 0; i < 16; i++){
                 std::cout << (outputs[i]+0);//(outputs[i] == true) ? "1" : "0";
             }
@@ -104,10 +113,19 @@ int main(){
     }
 }
 
+/* Sets up the interupt to make an exception */
+void setup_interupt(void){
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = sig_to_exception;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+}
+
 /* Checks if the error is a watchdog error
    If the error is a watchdog timeout, resets it and returns 0
    Otherwise return -1
-*/
+ */
 int check_watchdog(modbus_t *mb){
     if(errno != EMBXSFAIL)
         //Not a watchdog error
--- src/pulasan.h
+++ src/pulasan.h
@@ -3,6 +3,7 @@
 
 #include <modbus.h>
 
+void setup_interupt(void);
 int check_watchdog(modbus_t *mb);
 
 #endif /* PULASAN_H */
