bde7f420d72dcfb71e70be9b26e73fc7d113aa96
Legonigel <legonigel@gmail.com>
2015-11-09 23:07:47 -0500

    Get inputs working and slow down outputs

--- src/pulasan.cpp
+++ src/pulasan.cpp
@@ -7,11 +7,12 @@
 #include <exception>
 #include <unistd.h>
 #include <cerrno>
+#include <bitset>
 
 #define IP_ADDR "137.155.2.170"
 
-const int MODBUS_READ_ADDRESS = 100001;
-const int MODBUS_WRITE_ADDRESS = 1;
+const int MODBUS_READ_ADDRESS = 00000;
+const int MODBUS_WRITE_ADDRESS = 00000;
 
 class InterruptException : public std::exception
 {
@@ -46,17 +47,17 @@ int main(){
     sigIntHandler.sa_flags = 0;
     sigaction(SIGINT, &sigIntHandler, NULL);
 
-    uint8_t inputs[16] = {0};
+    uint16_t input = 0;
     uint8_t outputs[16] = {0};
-    bool write_bool = false;
-    int  write_num  = MODBUS_WRITE_ADDRESS;
+    bool write_bool = true;
+    int  write_num  = 0;
     int  count      = 0;
 
     std::cout << "Project Pulasan" << std::endl << "Press CTRL-C to exit" << std::endl << std::endl << std::endl << std::endl;
 
     try{
         while (true) {
-            int read_ret = modbus_read_input_bits(mb, MODBUS_READ_ADDRESS, 15, inputs);
+            int read_ret = modbus_read_registers(mb, MODBUS_READ_ADDRESS, 1, &input);
             if(read_ret == -1){
                 //Error status on read
                 if(check_watchdog(mb) == 0){
@@ -64,8 +65,8 @@ int main(){
                 }
                 fprintf(stderr, "Read failed: %s\n", modbus_strerror(errno));
                 break;
-                }
-            int write_ret = modbus_write_bits(mb, MODBUS_WRITE_ADDRESS-1, 16, outputs);
+            }
+            int write_ret = modbus_write_bits(mb, MODBUS_WRITE_ADDRESS, 16, outputs);
             if(write_ret == -1){
                 //Error status on write
                 if(check_watchdog(mb) == 0){
@@ -78,11 +79,7 @@ int main(){
             // use \e[A to move up a line
             std::cout << "\r\e[A\e[A\e[A"
                       << "         0123456789012345" << std::endl;
-            std::cout << "inputs:  ";
-            for(int i = 0; i < 16; i++){
-                std::cout << (inputs[i]+0);
-            }
-            std::cout << std::endl;
+            std::cout << "inputs:  " << std::bitset<16>(input) << std::endl;
             std::cout << "outputs: ";
             for(int i = 0; i < 16; i++){
                 std::cout << (outputs[i]+0);//(outputs[i] == true) ? "1" : "0";
@@ -90,12 +87,12 @@ int main(){
             std::cout << std::endl << std::flush;
 
             count++;
-            if(count==10){
+            if(count==50){
                 outputs[write_num] = write_bool;
                 count = 0;
                 write_num++;
             }
-            if(write_num == 16){
+            if(write_num==16){
                 write_bool = !write_bool;
                 write_num = 0;
             }
@@ -115,7 +112,6 @@ int check_watchdog(modbus_t *mb){
     if(errno != EMBXSFAIL)
         //Not a watchdog error
         return -1;
-
     uint16_t wd_status = 0;
     int read_status = modbus_read_registers(mb, 4108, 1, &wd_status);
     if (read_status == -1)
